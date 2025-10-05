165de5f619f760075f5e39349ad2ca17a456b218
Mohammad Arkam Ansari <mdarkamansari5@gmail.com>
2025-10-05 21:37:09 +0530

    Add files via upload

---
+++ nsg varibales .tf
@@ -0,0 +1,12 @@
+variable "nsg_name" {
+  type = string
+}
+variable "location" {
+  type = string
+}
+variable "resourcegroup_name" {
+  type = string
+}
+variable "app_subnetid" {
+  type = string
+}
