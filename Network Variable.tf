0bbf9af702080d6a5b8da4dcbea607dba0ec61c1
Mohammad Arkam Ansari <mdarkamansari5@gmail.com>
2025-10-05 22:19:38 +0530

    Add files via upload

---
+++ Network Variable.tf
@@ -0,0 +1,16 @@
+variable "vnet_name" {
+    type = string
+  
+}
+variable "location" {
+    type = string
+}
+variable "resourcegroup_name" {
+    type = string
+}
+variable "app_subnetid" {
+    type = string
+}
+variable "db_subnet" {
+    type = string
+}
